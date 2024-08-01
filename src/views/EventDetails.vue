87bdff56b63cd069dc2d1c763ebedf2a5f812d5a
parzivalchik <amiralybekturganov@gmail.com>
2024-08-01 14:53:52 +0500

    Update EventDetails.vue

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,28 +1,31 @@
+<script setup>
+import EventService from "@/services/EventService.js";
+import { computed, onMounted, ref } from "vue";
+
+const props = defineProps(["id"]);
+
+const event = ref("");
+const id = computed(() => props.id);
+onMounted(() => {
+  EventService.getEvent(id.value)
+    .then((response) => {
+      event.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+</script>
+
 <template>
   <div v-if="event">
+    <img 
+      :src="event.organizer" 
+      :alt="event.title" 
+      style="width: 1000px; height: 400px; object-fit: cover;"
+    />
     <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>Duration: {{ event.time }} Released: {{ event.date }} Watch on: {{ event.location }}</p>
     <p>{{ event.description }}</p>
   </div>
 </template>
-
-<script>
-import EventService from '@/services/EventService.js'
-export default {
-  props: ['id'],
-  data() {
-    return {
-      event: null
-    }
-  },
-  created() {
-    EventService.getEvent(this.id)
-      .then(response => {
-        this.event = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-}
-</script>
