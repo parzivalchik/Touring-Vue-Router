95f0f69903806cbf5d5bddda0b074ac581e81c06
parzivalchik <amiralybekturganov@gmail.com>
2024-08-01 14:48:01 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
-import About from '../views/About.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "@/views/EventList.vue";
+import EventDetails from "@/views/EventDetails.vue";
+import About from "@/views/About.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'EventList',
-    component: EventList
+    path: "/",
+    name: "EventList",
+    component: EventList,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: '/event/:id',
-    name: 'EventDetails',
+    path: "/event/:id",
+    name: "EventDetails",
     props: true,
-    component: EventDetails
+    component: EventDetails,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
